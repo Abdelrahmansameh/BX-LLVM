e56b8236746f20bf7cc192c1fa26b31e053c840b
Mamoun Elkhettar <mamoun.elkhettar@portugal.polytechnique.fr>
2019-12-19 14:02:53 +0100

    llvm

--- llvm.h
+++ llvm.h
@@ -21,7 +21,7 @@ using Label = std::string;
 // LLVM Assembly
 
 struct Llvm {
-  
+
   /** String of "destination", name of the operation */
   std::string dest;
 
@@ -40,9 +40,9 @@ struct Llvm {
    *   `t                -- types
    *   `a0, `a1, ...     -- arguments
    */
-  
+
   const std::string repr_template;
-  
+
   using ptr = std::shared_ptr<Llvm>;
 
 private:
@@ -51,66 +51,84 @@ private:
    * can be used to build instances
    */
   explicit Llvm(std::string const &dest, std::string const &type,
-               std::vector<Label> const &args, std::string const &repr)
+                std::vector<Label> const &args, std::string const &repr)
       : dest{dest}, type{type}, args{args}, repr_template{repr} {}
 
 public:
-
-static ptr set_label(int64_t imm) {
-    return std::unique_ptr<Llvm>(new Llvm{{}, {}, {}, std::to_string(imm)  + ":"});
+  static ptr set_label(int64_t imm) {
+    return std::unique_ptr<Llvm>(
+        new Llvm{{}, {}, {}, std::to_string(imm) + ":"});
   }
 
-#define ARITH_BINOP1(mnemonic)                                                                                                  \
-  static ptr mnemonic##q(std::string const &dest, std::string const &type, std::string const &arg1, std::string const &arg2) {  \
-    std::string repr = "\t %`d = " #mnemonic " nsw `t `a0, `a1";                                                                \
-    return std::shared_ptr<Llvm>(new Llvm{{dest}, {type}, {{arg1, arg2}}, repr});                                               \
-  }                                                                                                                             \
-  static ptr mnemonic##q(std::string const &dest, std::string const &type, int64_t imm, std::string const &arg2)  {             \
-    std::string repr = "\t %`d = " #mnemonic " nsw `t " + std::to_string(imm) + ", `a1";                                        \
-    return std::shared_ptr<Llvm>(new Llvm{{dest}, {type}, {arg2}, repr});                                                       \
-  }                                                                                                                             \
-  static ptr mnemonic##q(std::string const &dest, std::string const &type, std::string const &arg1, int64_t imm)  {             \
-    std::string repr = "\t %`d = " #mnemonic " nsw `t `a0, "+ std::to_string(imm);                                              \
-    return std::shared_ptr<Llvm>(new Llvm{{dest}, {type}, {arg1}, repr});                                                       \
+#define ARITH_BINOP1(mnemonic)                                                 \
+  static ptr mnemonic##q(std::string const &dest, std::string const &type,     \
+                         std::string const &arg1, std::string const &arg2) {   \
+    std::string repr = "\t %`d = " #mnemonic " nsw `t `a0, `a1";               \
+    return std::shared_ptr<Llvm>(                                              \
+        new Llvm{{dest}, {type}, {{arg1, arg2}}, repr});                       \
+  }                                                                            \
+  static ptr mnemonic##q(std::string const &dest, std::string const &type,     \
+                         int64_t imm, std::string const &arg2) {               \
+    std::string repr =                                                         \
+        "\t %`d = " #mnemonic " nsw `t " + std::to_string(imm) + ", `a1";      \
+    return std::shared_ptr<Llvm>(new Llvm{{dest}, {type}, {arg2}, repr});      \
+  }                                                                            \
+  static ptr mnemonic##q(std::string const &dest, std::string const &type,     \
+                         std::string const &arg1, int64_t imm) {               \
+    std::string repr =                                                         \
+        "\t %`d = " #mnemonic " nsw `t `a0, " + std::to_string(imm);           \
+    return std::shared_ptr<Llvm>(new Llvm{{dest}, {type}, {arg1}, repr});      \
   }
   ARITH_BINOP1(add)
   ARITH_BINOP1(sub)
   ARITH_BINOP1(mul)
 #undef ARITH_BINOP1
 
-#define ARITH_BINOP2(mnemonic)                                                                                                  \
-  static ptr mnemonic##q(std::string const &dest, std::string const &type, std::string const &arg1, std::string const &arg2) {  \
-    std::string repr = "\t %`d = " #mnemonic " `t `a0, `a1";                                                                    \
-    return std::shared_ptr<Llvm>(new Llvm{{dest}, {type}, {{arg1, arg2}}, repr});                                               \
-  }                                                                                                                             \
-  static ptr mnemonic##q(std::string const &dest, std::string const &type, int64_t imm, std::string const &arg2)  {             \
-    std::string repr = "\t %`d = " #mnemonic " `t " + std::to_string(imm) + ", `a1";                                            \
-    return std::shared_ptr<Llvm>(new Llvm{{dest}, {type}, {arg2}, repr});                                                       \
-  }                                                                                                                             \
-  static ptr mnemonic##q(std::string const &dest, std::string const &type, std::string const &arg1, int64_t imm)  {             \
-    std::string repr = "\t %`d = " #mnemonic " `t `a0, "+ std::to_string(imm);                                                  \
-    return std::shared_ptr<Llvm>(new Llvm{{dest}, {type}, {arg1}, repr});                                                       \
+#define ARITH_BINOP2(mnemonic)                                                 \
+  static ptr mnemonic##q(std::string const &dest, std::string const &type,     \
+                         std::string const &arg1, std::string const &arg2) {   \
+    std::string repr = "\t %`d = " #mnemonic " `t `a0, `a1";                   \
+    return std::shared_ptr<Llvm>(                                              \
+        new Llvm{{dest}, {type}, {{arg1, arg2}}, repr});                       \
+  }                                                                            \
+  static ptr mnemonic##q(std::string const &dest, std::string const &type,     \
+                         int64_t imm, std::string const &arg2) {               \
+    std::string repr =                                                         \
+        "\t %`d = " #mnemonic " `t " + std::to_string(imm) + ", `a1";          \
+    return std::shared_ptr<Llvm>(new Llvm{{dest}, {type}, {arg2}, repr});      \
+  }                                                                            \
+  static ptr mnemonic##q(std::string const &dest, std::string const &type,     \
+                         std::string const &arg1, int64_t imm) {               \
+    std::string repr =                                                         \
+        "\t %`d = " #mnemonic " `t `a0, " + std::to_string(imm);               \
+    return std::shared_ptr<Llvm>(new Llvm{{dest}, {type}, {arg1}, repr});      \
   }
   ARITH_BINOP2(udiv)
-  ARITH_BINOP2(shl) // left shift
+  ARITH_BINOP2(shl)  // left shift
   ARITH_BINOP2(ashr) // arithmetic right shift
   ARITH_BINOP2(and)
   ARITH_BINOP2(or)
-  ARITH_BINOP2(xor)
+  ARITH_BINOP2 (xor)
 #undef ARITH_BINOP2
-  
-#define COMP(mnemonic)                                                                                                          \
-  static ptr mnemonic##q(std::string const &dest, std::string const &type, std::string const &arg1, std::string const &arg2) {  \
-    std::string repr = "\t %`d = icomp " #mnemonic " `t `a0, `a1";                                                              \
-    return std::shared_ptr<Llvm>(new Llvm{{dest}, {type}, {{arg1, arg2}}, repr});                                               \
-  }                                                                                                                             \
-  static ptr mnemonic##q(std::string const &dest, std::string const &type, int64_t imm, std::string const &arg2)  {             \
-    std::string repr = "\t %`d = icomp " #mnemonic " `t " + std::to_string(imm) + ", `a1";                                      \
-    return std::shared_ptr<Llvm>(new Llvm{{dest}, {type}, {arg2}, repr});                                                       \
-  }                                                                                                                             \
-  static ptr mnemonic##q(std::string const &dest, std::string const &type, std::string const &arg1, int64_t imm)  {             \
-    std::string repr = "\t %`d = icomp " #mnemonic " `t `a0, "+ std::to_string(imm);                                            \
-    return std::shared_ptr<Llvm>(new Llvm{{dest}, {type}, {arg1}, repr});                                                       \
+
+#define COMP(mnemonic)                                                         \
+  static ptr mnemonic##q(std::string const &dest, std::string const &type,     \
+                         std::string const &arg1, std::string const &arg2) {   \
+    std::string repr = "\t %`d = icomp " #mnemonic " `t `a0, `a1";             \
+    return std::shared_ptr<Llvm>(                                              \
+        new Llvm{{dest}, {type}, {{arg1, arg2}}, repr});                       \
+  }                                                                            \
+  static ptr mnemonic##q(std::string const &dest, std::string const &type,     \
+                         int64_t imm, std::string const &arg2) {               \
+    std::string repr =                                                         \
+        "\t %`d = icomp " #mnemonic " `t " + std::to_string(imm) + ", `a1";    \
+    return std::shared_ptr<Llvm>(new Llvm{{dest}, {type}, {arg2}, repr});      \
+  }                                                                            \
+  static ptr mnemonic##q(std::string const &dest, std::string const &type,     \
+                         std::string const &arg1, int64_t imm) {               \
+    std::string repr =                                                         \
+        "\t %`d = icomp " #mnemonic " `t `a0, " + std::to_string(imm);         \
+    return std::shared_ptr<Llvm>(new Llvm{{dest}, {type}, {arg1}, repr});      \
   }
   COMP(eq)  // equal
   COMP(ne)  // not equal
@@ -120,69 +138,96 @@ static ptr set_label(int64_t imm) {
   COMP(sle) // signed less and equal
 #undef COMP
 
-static ptr global_with_value(std::string const &name, std::string const &type, int64_t imm) {                                 
-    std::string repr = "\t %`d = global `t " + std::to_string(imm) + ", align 8 ";
-    return std::unique_ptr<Llvm>(                                               
-        new Llvm{{name}, {type}, {}, repr}); 
+  static ptr global_with_value(std::string const &name, std::string const &type,
+                               int64_t imm) {
+    std::string repr =
+        "\t %`d = global `t " + std::to_string(imm) + ", align 8 ";
+    return std::unique_ptr<Llvm>(new Llvm{{name}, {type}, {}, repr});
   }
 
-static ptr global_no_value(std::string const &name, std::string const &type) {                                 
+  static ptr global_no_value(std::string const &name, std::string const &type) {
     std::string repr = "\t %`d = global `t, align 8 ";
-    return std::unique_ptr<Llvm>(                                               
-        new Llvm{{name}, {type}, {}, repr}); 
+    return std::unique_ptr<Llvm>(new Llvm{{name}, {type}, {}, repr});
   }
 
-static ptr ret_void() {                                 
+  static ptr ret_void() {
     std::string repr = "\t ret void";
-    return std::unique_ptr<Llvm>(                                               
-        new Llvm{{}, {}, {}, repr}); 
+    return std::unique_ptr<Llvm>(new Llvm{{}, {}, {}, repr});
   }
 
-static ptr ret_type(std::string const &type, std::string const &arg) {                                 
-    std::string repr = "\t ret `t "+arg;
-    return std::unique_ptr<Llvm>(                                               
-        new Llvm{{}, {type}, {}, repr}); 
+  static ptr ret_type(std::string const &type, std::string const &arg) {
+    std::string repr = "\t ret `t " + arg;
+    return std::unique_ptr<Llvm>(new Llvm{{}, {type}, {}, repr});
   }
 
-static ptr allocation(std::string const &name, std::string const &glb_var) {                                          
-    std::string repr = "\t %`d = alloca %"+ glb_var+ " align 8";
-    return std::unique_ptr<Llvm>(                                               
-        new Llvm{{name}, {}, {}, repr}); 
+  static ptr allocation(std::string const &name, std::string const &glb_var) {
+    std::string repr = "\t %`d = alloca %" + glb_var + " align 8";
+    return std::unique_ptr<Llvm>(new Llvm{{name}, {}, {}, repr});
   }
 
-/////////////////////////////////////////////////////////////////////////////////////////////
-/*
-static ptr alloca(std::string const &name) {                                          
-    std::string repr = "\t %`d = icomp   `t `a0, "+ std::to_string(imm);
-    return std::unique_ptr<Llvm>(                                               
-        new Llvm{{}, {dest}, {}, repr}); 
+  static ptr br_cond(std::string const &name, std::string const &fst, std::string const &snd) {
+    std::string repr = "\t br i1 %`d, label %" + fst + ", label %" + snd;
+    return std::unique_ptr<Llvm>(new Llvm{{name}, {}, {}, repr});
   }
 
-static ptr getelementptr(std::string const &name) {                                 
-    std::string repr = "\t %`d = icomp  `t `a0, "+ std::to_string(imm);
-    return std::unique_ptr<Llvm>(                                               
-        new Llvm{{}, {dest}, {}, repr}); 
+  static ptr br_uncond(std::string const &fst) {
+    std::string repr = "\t br label %" + fst;
+    return std::unique_ptr<Llvm>(new Llvm{{}, {}, {}, repr});
   }
 
-static ptr load(std::string const &name) {                                 
-    std::string repr = "\t %`d = icomp  `t `a0, "+ std::to_string(imm);
-    return std::unique_ptr<Llvm>(                                               
-        new Llvm{}, {dest}, {}, repr}); 
+  static ptr call(std::string const &name, std::string const &type, std::vector<std::vector<Label>> const &args) {
+    std::string repr = "\t call `t  @`d(";
+    if (!args.empty()){
+      int s = args.size();
+      for (int i=0; i<s; i++) {
+        repr = repr + args[i][0] + " " + args[i][1];
+      }
+    }
+    repr = repr + ")";
+    return std::unique_ptr<Llvm>(
+        new Llvm{{name}, {type}, {}, repr});
   }
 
-static ptr store(std::string const &name) {                                 
-    std::string repr = "\t %`d = icomp  `t `a0, "+ std::to_string(imm);
-    return std::unique_ptr<Llvm>(                                               
-        new Llvm{{}, {dest}, {}, repr}); 
+  static ptr define(std::string const &name, std::string const &type, std::vector<std::vector<Label>> const &args, std::string const &body) {
+    std::string repr = "define `t  @`d(";
+    if (!args.empty()){
+      int s = args.size();
+      for (int i=0; i<s; i++) {
+        repr = repr + args[i][0] + " " + args[i][1];
+      }
+    }
+    repr = repr + ") { \n" + body + "\n }";
+    return std::unique_ptr<Llvm>(
+        new Llvm{{name}, {type}, {}, repr});
   }
 
-static ptr call(std::string const &name) {                                 
-    std::string repr = "\t %`d = icomp  `t `a0, "+ std::to_string(imm);
-    return std::unique_ptr<Llvm>(                                               
-        new Llvm{}, {dest}, {}, repr}); 
-  }
-*/
 
+  /////////////////////////////////////////////////////////////////////////////////////////////
+  /*
+  static ptr alloca(std::string const &name) {
+      std::string repr = "\t %`d = icomp   `t `a0, "+ std::to_string(imm);
+      return std::unique_ptr<Llvm>(
+          new Llvm{{}, {dest}, {}, repr});
+    }
+
+  static ptr getelementptr(std::string const &name) {
+      std::string repr = "\t %`d = icomp  `t `a0, "+ std::to_string(imm);
+      return std::unique_ptr<Llvm>(
+          new Llvm{{}, {dest}, {}, repr});
+    }
+
+  static ptr load(std::string const &name) {
+      std::string repr = "\t %`d = icomp  `t `a0, "+ std::to_string(imm);
+      return std::unique_ptr<Llvm>(
+          new Llvm{}, {dest}, {}, repr});
+    }
+
+  static ptr store(std::string const &name) {
+      std::string repr = "\t %`d = icomp  `t `a0, "+ std::to_string(imm);
+      return std::unique_ptr<Llvm>(
+          new Llvm{{}, {dest}, {}, repr});
+    }
+  */
 };
 
 std::ostream &operator<<(std::ostream &out, Llvm const &line);
